a7716d5fa1515bc7080890e50c6ec3de4fb8fca7
Delermando Branquinho FIlho <delermando@gmail.com>
2016-11-10 22:01:56 -0200

    Coloring the graph

--- nearCentralit.R
+++ nearCentralit.R
@@ -9,7 +9,15 @@ net = rgraph(myData, mode = "graph")
 net = network(net, directed = FALSE)
 network.vertex.names(net) = 1:dim(myData)[1]
 par(new=F)
-ggnet2(net,color = "blue", label = TRUE, label.color = "white")
+palette <- c("purple","blue","green","yellow","orange","red")
+
+c.d   <- degree(net)
+col<- as.integer(5*(c.d-min(c.d))/diff(range(c.d))+1)
+
+g<-ggnet2(net, label = TRUE, label.color = "white",color = palette[col])
+
+plot(g,vertex.color=palette[col],main="Degree Centrality",
+     layout=layout.fruchterman.reingold)
 
 degree(net,cmode="indegree")
 degree(net,cmode="outdegree")
